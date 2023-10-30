3a22fc688879e45a9e6b5896d1ec6e43281b0925
Hasindu Bandara <87415515+HasinduBandara@users.noreply.github.com>
2023-10-30 23:26:39 +0530

    backend blog

--- BlogController.js
+++ BlogController.js
@@ -2,112 +2,111 @@ const blogModel = require("../Models/BlogModel");
 
 //insert blog
 const blogInsert = async (req, res) => {
-    const {
-        blogID,
-        bloggerName,
-        bloggerPosition,
-        title,
-       /*  titleo, */
-        content,
-    } = req.body;
+  const { blogID, bloggerName, bloggerPosition, title, content } = req.body;
+  // Find the latest blog with the highest blogID
+  const latestBlog = await blogModel.findOne().sort({ blogID: -1 });
+  let nextBlogID = 1;
+  // Default value if no blogs exist
+  if (latestBlog) {
+    // If there's a latest blog, increment its blogID by 1
+    nextBlogID = latestBlog.blogID + 1;
+  }
 
-    const BlogItem = req.files.file;
-    const BlogNameStore = new Date().getTime();
-    await BlogItem.mv("Assets/Bloggers/" + `${BlogNameStore}.jpg`, (err) => {
-        console.log("This is the file error::: ", err);
-    })
-    console.log("File name is ", BlogNameStore)
+  const BlogItem = req.files.file;
+  const BlogNameStore = new Date().getTime();
+  await BlogItem.mv("Assets/Bloggers/" + `${BlogNameStore}.jpg`, (err) => {
+    console.log("This is the file error::: ", err);
+  });
+  console.log("File name is ", BlogNameStore);
 
-    try {
-        const blogs = new blogModel({
-            blogID,
-            bloggerName,
-            bloggerPosition,
-            title,
-           // titleo, //ss
-            content,
-            bloggerImage: `${BlogNameStore}.jpg`
-        });
-        return await blogs
-            .save()
-            .then((value) => {
-                return res.status(201).json({ ID: value._id })
-            }).catch((err) => {
-                return res.status(500).json({ err });
-            })
-    } catch (error) {
-        console.log("Error", error);
-        res.status(400).json({ error: error.message })
-    }
+  try {
+    const blogs = new blogModel({
+      blogID:nextBlogID,
+      bloggerName,
+      bloggerPosition,
+      title,
+      content,
+      bloggerImage: `${BlogNameStore}.jpg`,
+    });
+    return await blogs
+      .save()
+      .then((value) => {
+        return res.status(201).json({ ID: value._id });
+      })
+      .catch((err) => {
+        return res.status(500).json({ err });
+      });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //get all blog details
 const getBlogDetails = async (req, res) => {
-    try {
-        const blogs = await blogModel.find();
-        return res.status(200).json(blogs);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ err: err.message })
-    }
-}
+  try {
+    const blogs = await blogModel.find();
+    return res.status(200).json(blogs);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: err.message });
+  }
+};
 
 //get the selected blog view
 const getBlogDetailsView = async (req, res) => {
-    try {
-        const ID = req.params.id;
-        const blog = await blogModel.find({ _id: ID });
-        res.status(200).send({ status: "Blog data recieved", BlogDetails: blog });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Server error");
-    }
-}
+  try {
+    const ID = req.params.id;
+    const blog = await blogModel.find({ _id: ID });
+    res.status(200).send({ status: "Blog data recieved", BlogDetails: blog });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+};
 
 //delete a specific blog
 const deleteBlog = async (req, res) => {
-    const id = req.params.id;
-    await blogModel.findByIdAndDelete(id).then(() => {
-        res.status(200).send({ state: "Success" });
-    }).catch((err) => {
-        res.status(400).send({ send: err });
+  const id = req.params.id;
+  await blogModel
+    .findByIdAndDelete(id)
+    .then(() => {
+      res.status(200).send({ state: "Success" });
     })
-}
+    .catch((err) => {
+      res.status(400).send({ send: err });
+    });
+};
 
 //upadate a selected blog detail
 const blogUpdate = async (req, res) => {
-    const id = req.body.id
-    const {
-        blogID,
-        bloggerName,
-        bloggerPosition,
-        title,
-        /* titleo, */
-        content,
-    } = req.body;
+  const id = req.body.id;
+  const { blogID, bloggerName, bloggerPosition, title, content } = req.body;
 
-    console.log("ID:::", id)
+  console.log("ID:::", id);
 
-    const newData = {
-        blogID,
-        bloggerName,
-        bloggerPosition,
-        title,
-     /*    titleo, */
-        content,
-    };
+  const newData = {
+    blogID,
+    bloggerName,
+    bloggerPosition,
+    title,
+    content,
+  };
 
-    await blogModel.findByIdAndUpdate(id, newData).then(() => {
-        res.status(200).send({ state: "Update", data: newData });
-    }).catch((err) => {
-        res.status(400).send({ state: err });
+  await blogModel
+    .findByIdAndUpdate(id, newData)
+    .then(() => {
+      res.status(200).send({ state: "Update", data: newData });
     })
-}
+    .catch((err) => {
+      res.status(400).send({ state: err });
+    });
+};
 
 module.exports = {
-    blogInsert,
-    getBlogDetails,
-    getBlogDetailsView,
-    deleteBlog,
-    blogUpdate
-}
+  blogInsert,
+  getBlogDetails,
+  getBlogDetailsView,
+  deleteBlog,
+  blogUpdate,
+};
